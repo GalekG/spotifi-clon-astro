31979a6369479a54f808cdc8319a193de46b9a2b
Kevin Alexander Checa Díaz <alebean99@gmail.com>
2023-12-20 21:05:21 -0500

    added: aside | main | media-player | playlist detail

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import svelte from "@astrojs/svelte";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   i18n: {
     defaultLocale: 'es',
     locales: ['es', 'en']
-  }
+  },
+  integrations: [tailwind(), svelte(), react()],
+  output: 'server'
 });
